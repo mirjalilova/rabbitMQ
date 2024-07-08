677a72d24c200500ad8fc0249dcef6e7e6973869
feruza17 <feruzamirjalilova767@gmail.com>
2024-07-08 11:26:20 +0500

    ;

---
+++ 3/consumer/consumen.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+        "log"
+
+        amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+        if err != nil {
+                log.Panicf("%s: %s", msg, err)
+        }
+}
+
+func main() {
+        conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+        failOnError(err, "Failed to connect to RabbitMQ")
+        defer conn.Close()
+
+        ch, err := conn.Channel()
+        failOnError(err, "Failed to open a channel")
+        defer ch.Close()
+
+        err = ch.ExchangeDeclare(
+                "logs", 
+                "fanout",
+                true,    
+                false,  
+                false,  
+                false, 
+                nil,    
+        )
+        failOnError(err, "Failed to declare an exchange")
+
+        q, err := ch.QueueDeclare(
+                "",   
+                false,
+                false, 
+                true,  
+                false,
+                nil,   
+        )
+        failOnError(err, "Failed to declare a queue")
+
+        err = ch.QueueBind(
+                q.Name, 
+                "",   
+                "logs",
+                false,
+                nil,
+        )
+        failOnError(err, "Failed to bind a queue")
+
+        msgs, err := ch.Consume(
+                q.Name,
+                "",    
+                true,
+                false, 
+                false, 
+                false,
+                nil,    
+        )
+        failOnError(err, "Failed to register a consumer")
+
+        var forever chan struct{}
+
+        go func() {
+                for d := range msgs {
+                        log.Printf(" [x] %s", d.Body)
+                }
+        }()
+
+        log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+        <-forever
+}
